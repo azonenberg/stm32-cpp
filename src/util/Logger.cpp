98ce322a25df727daf21051b1a4ffb35ac59fb61
Andrew Zonenberg <azonenberg@drawersteak.com>
2024-04-24 22:11:04 -0700

    Logger: initial RTC timestamping support

--- src/util/Logger.cpp
+++ src/util/Logger.cpp
@@ -1,8 +1,8 @@
 /***********************************************************************************************************************
 *                                                                                                                      *
-* STM32-CPP v0.1                                                                                                       *
+* STM32-CPP                                                                                                            *
 *                                                                                                                      *
-* Copyright (c) 2020-2023 Andrew D. Zonenberg                                                                          *
+* Copyright (c) 2020-2024 Andrew D. Zonenberg                                                                          *
 * All rights reserved.                                                                                                 *
 *                                                                                                                      *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
@@ -31,26 +31,53 @@
 #include <stdint.h>
 #include "Logger.h"
 
+#ifdef LOGGER_USE_RTC_TIMESTAMP
+#include <peripheral/RTC.h>
+#endif
+
 #ifdef HAVE_TIM
 
 void Logger::Timestamp(LogType type)
 {
-	//TODO: support printf %ld and use int64 here
-	uint32_t uptime = (m_timeOffset + m_timer->GetCount()) / 10;
+	//Print the prefix for the log message
 	switch(type)
 	{
 		case NORMAL:
-			m_target->Printf("[\033[32m%8d.%03d\033[0m] ", uptime / 1000, uptime % 1000);
+			m_target->Printf("[\033[32m");
 			break;
 
 		case WARNING:
-			m_target->Printf("[\033[33;1m%8d.%03d\033[0m] ", uptime / 1000, uptime % 1000);
+			m_target->Printf("[\033[33;1m");
 			break;
 
 		case ERROR:
-			m_target->Printf("[\033[31;1m%8d.%03d\033[0m] ", uptime / 1000, uptime % 1000);
+			m_target->Printf("[\033[31;1m");
 			break;
 	}
+
+	#ifdef LOGGER_USE_RTC_TIMESTAMP
+
+		//this assumes RTC sub-second counter is 10 kHz ticks for now, should we find a way to change this later?
+		tm rtctime;
+		uint16_t rtcsubsec;
+		RTC::GetTime(rtctime, rtcsubsec);
+
+		m_target->Printf("%04d-%02d-%02dT%02d:%02d:%02d.%04d\033[0m] ",
+			rtctime.tm_year + 1900,
+			rtctime.tm_mon+1,
+			rtctime.tm_mday,
+			rtctime.tm_hour,
+			rtctime.tm_min,
+			rtctime.tm_sec,
+			rtcsubsec);
+
+	#else
+
+		//TODO: support printf %ld and use int64 here
+		uint32_t uptime = (m_timeOffset + m_timer->GetCount()) / 10;
+		m_target->Printf("%8d.%03d\033[0m] ", uptime / 1000, uptime % 1000);
+
+	#endif
 }
 
 void Logger::PrintIndent()
