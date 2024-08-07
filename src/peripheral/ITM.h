fa26036e8e47a51306d64dacb3bc0f3c665b12f1
Andrew Zonenberg <azonenberg@drawersteak.com>
2024-08-06 22:20:09 -0700

    More work on ITM wrappers

--- src/peripheral/ITM.h
+++ src/peripheral/ITM.h
@@ -27,45 +27,48 @@
 *                                                                                                                      *
 ***********************************************************************************************************************/
 
-#ifndef stm32_itm_h
-#define stm32_itm_h
+#ifndef itm_h
+#define itm_h
 
-enum demcr_t
+/**
+	@brief The Instrumentation Trace Macrocell
+ */
+class ITM
 {
-	DEMCR_TRCENA	= 0x0100'0000
+public:
+
+	/**
+		@brief Unlock the ITM
+	 */
+	static void Unlock()
+	{ _ITM.LAR = ITM_LAR_UNLOCK; }
+
+	/**
+		@brief Enable the ITM (also turns on trace support globally)
+	 */
+	static void Enable()
+	{
+		Unlock();
+
+		//Enable tracing system wide (turns on DWT and ITM)
+		DEMCR |= DEMCR_TRCENA;
+
+		//Enable the ITM itself
+		_ITM.TCR |= ITM_TCR_ITMENA;
+	}
+
+	/**
+		@brief Enable an ITM stimulus channel
+	 */
+	static void EnableChannel(uint8_t chan)
+	{ _ITM.TER[chan / 32] |= (1 << (chan % 32)); }
+
+	/**
+		@brief Enable forwarding of hardware events from the DWT to the ITM and TPIU
+	 */
+	static void EnableDwtForwarding()
+	{ _ITM.TCR |= ITM_TCR_TXENA; }
+
 };
 
-enum itm_tcr_t
-{
-	ITM_TCR_TXENA	= 0x0000'0008,
-	ITM_TCR_ITMENA	= 0x0000'0001
-};
-
-enum itm_lar_t
-{
-	ITM_LAR_UNLOCK	= 0xC5ACCE55
-};
-
-struct itm_t
-{
-	uint32_t STIM[256];
-	uint32_t field_400[640];
-	uint32_t TER[8];
-	uint32_t field_e20[8];
-	uint32_t TPR;
-	uint32_t field_e44[15];
-	uint32_t TCR;
-
-	//Cortex-M7 specific, TODO figure this out for other chips?
-	uint32_t field_e84[27];
-	uint32_t ITATRDY;
-	uint32_t field_ef4;
-	uint32_t ITATVAL;
-	uint32_t field_efc;
-	uint32_t TCTRL;
-	uint32_t field_f04[43];
-	uint32_t LAR;
-	uint32_t LSR;
-};
-
-#endif	//include guard
+#endif
