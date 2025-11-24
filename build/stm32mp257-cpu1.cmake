4e9035f633774be79ed8f79c3788f5cd454428db
Andrew Zonenberg <azonenberg@drawersteak.com>
2025-11-23 22:58:23 -0800

    Updated linker scripts with proper base stack address w/ no offset

---
+++ build/stm32mp257-cpu1.cmake
@@ -0,0 +1,18 @@
+########################################################################################################################
+# Definitions for this target come first
+# TODO: KVS definitions need to be updated since no internal flash??
+
+add_compile_definitions(STM32MP257)
+add_compile_definitions(STM32MP2_CPU1)
+add_compile_definitions(MULTICORE)
+add_compile_definitions(MICROKVS_WRITE_BLOCK_SIZE=32)
+add_compile_definitions(KVS_NAMELEN=32)
+
+# Platform flags
+set(ARCHFLAGS "-mcpu=cortex-a35 -march=armv8-a+simd -mfloat-abi=hard")
+set(ARCHFLAGS "${ARCHFLAGS} -fno-exceptions -fno-rtti --specs=nano.specs -fno-threadsafe-statics")
+
+########################################################################################################################
+# Common build flags at the end since they pull in ARCHFLAGS
+
+include("${CMAKE_CURRENT_LIST_DIR}/common.cmake")
